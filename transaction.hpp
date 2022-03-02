38d429e33b2251cefff7ea2b37c10728bbea4715
Ptyg <nawkking@gmail.com>
2022-03-02 16:19:49 +0200

    Add public getters in transaction class

---
+++ transaction.hpp
@@ -0,0 +1,21 @@
+#ifndef TRANSACTION
+#define TRANSACTION
+
+#ifndef STRING
+#define STRING
+#include <string>
+#endif
+
+class Transaction{
+    size_t coin_number;
+    std::string sender, recipient;
+    time_t create_time;
+public:
+    void display_transaction_info() const;
+    const size_t& get_coin_number() const;
+    const std::string& get_sender() const;
+    const std::string& get_recipient() const;
+    const time_t& get_create_time() const;
+};
+
+#endif TRANSACTION
